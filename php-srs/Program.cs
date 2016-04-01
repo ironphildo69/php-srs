6f30f77bdfb72d183a7e920d8ca25eac7f3d6c23
Phillip <iron_phildo69@hotmail.com>
2016-04-01 15:36:56 +1100

    Fixed typo

--- php-srs/Program.cs
+++ php-srs/Program.cs
@@ -10,7 +10,7 @@ namespace php_srs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("tst for initial branch");
+            Console.WriteLine("test for initial branch");
         }
     }
 }
