5784ed92ca5e1587cc7ae64fb9f80c4e8c72e3b7
abuhary <abuhary@live.com>
2016-04-04 22:09:28 +1000

    added command line gui

--- php-srs/Program.cs
+++ php-srs/Program.cs
@@ -10,12 +10,6 @@ namespace php_srs
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("test for initial branch");
-            checkDatabaseFile();
-        }
-        
         static void checkDatabaseFile() //method that checks for the database file and if it doesnt exist creates it.
         {
             if (File.Exists("php-srs_database.sqlite"))
@@ -33,5 +27,53 @@ namespace php_srs
         {
             //code here
         }
+
+        static void Main(string[] args)
+        {
+            int selection = 0;
+
+            while (selection != 5)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("**************************************");
+                Console.WriteLine("*                                    *");
+                Console.WriteLine("******* PEOPLE HEALTH PHARMACY *******");
+                Console.WriteLine("******* SALES REPORTING SYSTEM *******");
+                Console.WriteLine("**************************************");
+                Console.WriteLine("*                                    *");
+                Console.WriteLine("* 1:  Add Sales Record               *");
+                Console.WriteLine("* 2:  View Stock                     *");
+                Console.WriteLine("* 3:  Output to CSV file             *");
+                Console.WriteLine("* 4:  Log Out                        *");
+                Console.WriteLine("* 5:  Quit                           *");
+                Console.WriteLine("*                                    *");
+                Console.WriteLine("**************************************");
+                Console.WriteLine("Please Enter Option :  ");
+
+                selection = int.Parse(Console.ReadLine());
+
+                switch (selection)
+                {
+                    case 1:
+                        Console.WriteLine("add sales record");      //relevant methods will be called depending on the users selection
+                        break;
+
+                    case 2:
+                        Console.WriteLine("view stock");        //relevant methods will be called depending on the users selection
+                        checkDatabaseFile();
+                        break;
+                    case 3:
+
+                        Console.WriteLine("CSV");       //relevant methods will be called depending on the users selection
+                        break;
+
+                    case 4:
+                        Console.WriteLine("log out");       //relevant methods will be called depending on the users selection
+                        break;
+                }
+            }
+        }
+        
+        
     }
 }
