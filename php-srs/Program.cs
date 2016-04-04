dab79c79aae967dc4fb296e324352741cba2474f
abuhary <abuhary@live.com>
2016-04-04 22:40:12 +1000

    added medicine class

--- php-srs/Program.cs
+++ php-srs/Program.cs
@@ -25,7 +25,7 @@ namespace php_srs
 
         static void addSalesRecord() //Needs work, also needs to accept a enumorated type and send it to the SQL database. Phil will do SQL statements.
         {
-            //code here
+           // code here
         }
 
         static void Main(string[] args)
