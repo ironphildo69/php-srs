9e1467919941305a15dd9e9fd447ebef725842cc
abuhary <abuhary@live.com>
2016-04-08 00:44:54 +1000

    added command line to project

--- src/php_srs.cs
+++ src/php_srs.cs
@@ -3,36 +3,11 @@ using System.IO;
 using SwinGameSDK;
 using System.Data.SQLite;
 
+
 namespace php_srs
 {
     public class GameMain
     {
-        public static void Main()
-        {
-            //Pre initalisation logic
-
-            createDatabase();
-
-            //Open the game window
-            
-            SwinGame.OpenGraphicsWindow("GameMain", 800, 600);
-            SwinGame.ShowSwinGameSplashScreen();
-            
-            //Run the game loop
-            while(false == SwinGame.WindowCloseRequested())
-            {
-                //Fetch the next batch of UI interaction
-                SwinGame.ProcessEvents();
-                
-                //Clear the screen and draw the framerate
-                SwinGame.ClearScreen(Color.White);
-                SwinGame.DrawFramerate(0,0);
-                
-                //Draw onto the screen
-                SwinGame.RefreshScreen(60);
-            }
-        }
-
         //Global Variables
         public static void globalVariables()
         {
@@ -56,6 +31,10 @@ namespace php_srs
         }
 
         //Start of sales record function
+        static void addSalesRecord() //Needs work, also needs to accept a enumorated type and send it to the SQL database. Phil will do SQL statements.
+        {
+            // code here
+        }
 
         //CODE HERE
 
@@ -63,5 +42,53 @@ namespace php_srs
 
         //CODE HERE
 
+        public static void Main(string[] args)
+        {
+            //Pre initalisation logic
+            createDatabase();
+
+            int selection = 0;
+
+            //Menu selection loop
+            while (selection != 5)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("**************************************");
+                Console.WriteLine("*                                    *");
+                Console.WriteLine("******* PEOPLE HEALTH PHARMACY *******");
+                Console.WriteLine("******* SALES REPORTING SYSTEM *******");
+                Console.WriteLine("**************************************");
+                Console.WriteLine("*                                    *");
+                Console.WriteLine("* 1:  Add Sales Record               *");
+                Console.WriteLine("* 2:  View Stock                     *");
+                Console.WriteLine("* 3:  Output to CSV file             *");
+                Console.WriteLine("* 4:  Log Out                        *");
+                Console.WriteLine("* 5:  Quit                           *");
+                Console.WriteLine("*                                    *");
+                Console.WriteLine("**************************************");
+                Console.WriteLine("Please Enter Option :  ");
+
+                selection = int.Parse(Console.ReadLine());
+
+                switch (selection)
+                {
+                    case 1:
+                        Console.WriteLine("add sales record");      //relevant methods will be called depending on the users selection
+                        break;
+
+                    case 2:
+                        Console.WriteLine("view stock");        //relevant methods will be called depending on the users selection
+                        break;
+                    case 3:
+
+                        Console.WriteLine("CSV");       //relevant methods will be called depending on the users selection
+                        break;
+
+                    case 4:
+                        Console.WriteLine("log out");       //relevant methods will be called depending on the users selection
+                        break;
+                }
+            }
+        }
     }
 }
