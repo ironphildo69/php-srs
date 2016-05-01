c13fa629320d58f0f2bf86de035ff92b998e60e8
Phillip <Phillip@PHILDO-DESKTOP>
2016-05-01 17:07:06 +1000

    Started Report

--- php-srs/Report.cs
+++ php-srs/Report.cs
@@ -10,18 +10,31 @@ namespace php_srs
 {
     class Report
     {
-        public static void SalesReport()
+        private void saleReport()
         {
+            TextWriter tw = new StreamWriter("CSV-StockSales.txt");
+            double total = 0.0;
+
+            tw.WriteLine("PEOPLE HEALTH PHARMACY - SALES REPORTING SYSTEM\n");
+            tw.WriteLine("SALES RECORDS REPORT\n");
+            tw.WriteLine("------------------------------------------------------------------------------------------------------------------------\n");
+
             var php_srsConnection = new SQLiteConnection("Data Source=php-srs_database.sqlite;Version=3;"); //Prepares the connection to the database
             php_srsConnection.Open();   //Opens the connection
 
-        }
+            string query = "SELECT * FROM SalesRecords"; //Acquires the SQL SELECT statement from the reference
 
-        public static void StockReport()
-        {
-            var php_srsConnection = new SQLiteConnection("Data Source=php-srs_database.sqlite;Version=3;"); //Prepares the connection to the database
-            php_srsConnection.Open();   //Opens the connection
+            SQLiteCommand selectCommand = new SQLiteCommand(query, php_srsConnection);  //Sets up the query to be used with the database
+            SQLiteDataReader readResults = selectCommand.ExecuteReader();   //Reads the results of the query into something that can be easily manipulated
+            while (readResults.Read())
+            {
+                tw.WriteLine(readResults["ID"] + ", " + readResults["Name"] + ", " + readResults["Description"] + ", " + readResults["Attribute"] + ", " +
+                    readResults["Quantity"] + ", " + readResults["Date"] + "\n");
+                total = total + (double) readResults["Price"];
+            }
 
+            tw.Close();
+            php_srsConnection.Close();  //Closes the connection
         }
     }
 }
